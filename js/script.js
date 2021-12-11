ec230c59e03614febfad93c211cec4b9e08e54aa
frame88 <fleuzzi97@gmail.com>
2021-12-11 17:49:29 +0100

    new cards js

--- js/script.js
+++ js/script.js
@@ -31,23 +31,68 @@ const team = [
   },
 ];
 
+
+
 /*
 In allegato trovate un layout di base e un file js in cui è definito un array di oggetti che rappresentano i membro del team.
 Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
 */
-
-
 //creare una sola carta e usarla come modello per le successive
 //i dati da usare non vengono dati in input, poichè sono gia qui sopra
 //
+/*
+const inputClass = document.getElementById('class');
+const inputTeacher = document.getElementById('teacher');
+const button = document.getElementById('addClass');
 
+button.addEventListener('click', function() {
+  const nameClass = inputClass.value;
+  const nameTeacher = inputTeacher.value;
+  const obj = {
+    name : nameClass,
+    teacher : nameTeacher,
+  };
+  console.log(obj);
+  classiArrobj
+  
+})
+*/
 
+/*
+        < div class="team-container" >
+          < !--Inseriamo inizialmente una card statica per stilarla e vedere l'effetto finale -->
+          < div class="team-card" >
+            <div class="card-image">
+              <img
+                src="img/wayne-barnett-founder-ceo.jpg"
+                alt="Wayne Barnett"
+              />
+            </div>
+            <div class="card-text">
+              <h3>Wayne Barnett</h3>
+              <p>Founder & CEO</p>
+            </div>
+          </div >
+        </div >
 
+*/
 
-
-
-
+for (let i = 0; i < team.length; i++) {
+  const member = team[i];
+  const template = < div class="team-card" >
+    <div class="card-image">
+      <img
+        src="img/${member.image}"
+        alt="${member.nome}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${member.nome}</h3>
+      <p>${member.role}</p>
+    </div>
+  </div >
+}
 
 
 
